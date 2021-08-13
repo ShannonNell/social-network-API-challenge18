b56b66ac16f3e31e9c09bbd77198017df48533a4
Shannon <shannon.nell92@gmail.com>
2021-08-13 18:50:05 -0400

    5 main CRUD for /api/users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -47,6 +47,19 @@ const userController = {
             })
             .catch(err => res.status(400).json(err));
     },
+
+    // DELETE user by id --> /api/users/:id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 };
 
 module.exports = userController;
